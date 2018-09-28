9cd4da9035830a697f3e9cdea1a4ca5a3780d6a3
David Tang <dtang@atlassian.com>
2018-09-28 16:26:47 -0700

    Add all exports to index.js

--- src/index.js
+++ src/index.js
@@ -1,1 +1,4 @@
 export { mockSchema } from "./mock-schema";
+export { mockApolloClient } from "./mock-apollo-client";
+export { MockApolloProvider } from "./mock-apollo-provider";
+export { SchemaController } from "./schema-controller";
